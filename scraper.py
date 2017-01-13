1fa970599fcc0e45528bf02b0f9cc072f15e72f0
Michael Nguyen <michaelnguyenm@gmail.com>
2017-01-13 17:26:30 -0600

    Retrieved anime id from html, closes #20

--- scraper.py
+++ scraper.py
@@ -1,10 +1,21 @@
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import os.path
+import time
+
+#
+def get_adb_id(adb_link):
+    url = urlparse(adb_link)
+    path = url.path
+    sliced_path = os.path.split(path)
+    temp_id = sliced_path[1]
+    return temp_id[1:]
 
 # Consider not hardcoding the file from the local directory
 soup = BeautifulSoup(open('anime.html'), 'html.parser')
 
 # interactive python3, use exec(open('scraper.py').read())
-title_tags_list = soup.find_all('h3')
+title_tag_list = soup.find_all('h3')
 for line in title_tag_list:
     # Entire tag
     # print(line)
@@ -16,6 +27,10 @@ for line in title_tag_list:
     title_rom = line.get('data-romaji')
     title_en = line.get('data-english')
     title_other = line.get('data-alternate')
-    # Get air date
-    # Get MAL link
+    # Get links
+    # Need to make tests for when these classes don't exist
     mal_link = line.find_next('a', {'class':'mal-icon'}).get('href')
+    adb_link = line.find_next('a', {'class':'anidb-icon'}).get('href')
+    # Process link for ID
+    adb_id = get_adb_id(adb_link)
+    # Look up ID on anidb if it exists
